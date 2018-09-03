220ab65a287a12552e3349910a97b5ab0a3730af
Nicholas Eduardo <nicholasess@gmail.com>
2018-09-03 16:19:25 -0300

    fix with hashRouter

--- doczrc.js
+++ doczrc.js
@@ -2,5 +2,6 @@ const doczPluginGithubPage = require('docz-plugin-github-page');
 export default {
   src: './documentation',
   base: "/react-native-share/",
+  hashRouter: true,
   plugins: [doczPluginGithubPage()],
 };
