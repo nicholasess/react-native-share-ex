274615ceffc1aba08a30ac1cafbdc47d30df7f3a
Nicholas Eduardo <nicholasess@gmail.com>
2018-09-12 16:26:00 -0300

    fix(docz): update docz

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,7 @@
 const doczPluginGithubPage = require('docz-plugin-github-page');
 export default {
   src: './documentation',
-  base: "/react-native-share/",
+  base: '/react-native-share-ex/',
   hashRouter: true,
   plugins: [doczPluginGithubPage()],
 };
